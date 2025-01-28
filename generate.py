e6b550dc52339801f8791e3d65d0146cdcae69e3
cwcrowel <cwcrowel@uvm.edu>
2025-01-27 20:34:25 -0500

    one block tower

--- generate.py
+++ generate.py
@@ -1,5 +1,22 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+length = 1
+width = 1
+height = 1
+x = 0
+y = 0
+z = 0.5
+
+pyrosim.Start_SDF("boxes.sdf")
+for i in range(10):
+    pyrosim.Send_Cube(name="Box", pos=[x+i, y, z] , size=[length, width, height])
+    # Reposition
+    length = length * 0.9
+    width = width * 0.9
+    height = height * 0.9
+    # Resize
+    x = x - 1
+    y = y
+    z = z + 1
+
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -1,10 +1,14 @@
 import pybullet as p
 import time
+import pybullet_data
 
 physicsClient = p.connect(p.GUI)
 p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-p.loadSDF("box.sdf")
+p.setGravity(0,0,-9.8,physicsClient)
+planeId = p.loadURDF("plane.urdf")
+p.loadSDF("boxes.sdf")
 
 for i in range(1000):
     p.stepSimulation()
