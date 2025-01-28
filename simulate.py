45fccaedb92f24727fdaec080f20e59ebe646414
cwcrowel <cwcrowel@uvm.edu>
2025-01-27 19:53:30 -0500

    box inserted in simulation

--- simulate.py
+++ simulate.py
@@ -2,10 +2,13 @@ import pybullet as p
 import time
 
 physicsClient = p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+
+p.loadSDF("box.sdf")
 
 for i in range(1000):
     p.stepSimulation()
-    time.sleep(0.1)
+    time.sleep(0.05)
     print(i)
 
 p.disconnect()
