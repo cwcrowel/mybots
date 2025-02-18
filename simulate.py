7f464a4bc2627461447dfd13144de905f0df9f40
cwcrowel <cwcrowel@uvm.edu>
2025-02-17 20:54:13 -0500

    Assignment 5 - completed

--- simulate.py
+++ simulate.py
@@ -1,17 +1,19 @@
 import pybullet as p
 import time
 import pybullet_data
-from salt.modules.sysfs import target
 
 import pyrosim.pyrosim as pyrosim
 import numpy
-import random
+#import random
 
-from analyze import targetAnglesValues
 
-amplitude = numpy.pi/4
-frequency = 1
-phaseOffset = 0
+
+BackLeg_amplitude = numpy.pi/4
+BackLeg_frequency = 20
+BackLeg_phaseOffset = numpy.pi
+FrontLeg_amplitude = numpy.pi/4
+FrontLeg_frequency = 20
+FrontLeg_phaseOffset = numpy.pi/3
 
 physicsClient = p.connect(p.GUI)
 p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
@@ -26,8 +28,13 @@ pyrosim.Prepare_To_Simulate(robotId)
 backLegSensorValues = numpy.zeros(1000)
 frontLegSensorValues = numpy.zeros(1000)
 
-targetAngles = numpy.sin(numpy.linspace(0, (numpy.pi*2), 1000)) * (numpy.pi/4)
-#numpy.save('data/ta.npy', targetAngles)
+targetAngles = numpy.linspace(-numpy.pi, numpy.pi, 1000)
+BackLeg_out_array = BackLeg_amplitude * numpy.sin(BackLeg_frequency * targetAngles + BackLeg_phaseOffset)
+FrontLeg_out_array = FrontLeg_amplitude * numpy.sin(FrontLeg_frequency * targetAngles + FrontLeg_phaseOffset)
+
+#numpy.save('data/blsv2.npy', BackLeg_out_array)
+#numpy.save('data/flsv2.npy', FrontLeg_out_array)
+#exit()
 
 for i in range(1000):
     p.stepSimulation()
@@ -38,14 +45,14 @@ for i in range(1000):
         bodyIndex= robotId,
         jointName= b'Torso_BackLeg',
         controlMode= p.POSITION_CONTROL,
-        targetPosition= targetAngles[i],
+        targetPosition= BackLeg_out_array[i],
         maxForce= 30)
 
     pyrosim.Set_Motor_For_Joint(
         bodyIndex=robotId,
         jointName=b'Torso_FrontLeg',
         controlMode=p.POSITION_CONTROL,
-        targetPosition= targetAngles[i],
+        targetPosition= FrontLeg_out_array[i],
         maxForce=30)
 
     time.sleep(0.01)
