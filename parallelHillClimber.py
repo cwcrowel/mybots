858fbfa45f46439552b8fe59c929aac0b3d66c4d
cwcrowel <cwcrowel@uvm.edu>
2025-03-18 22:34:05 -0400

    Assignment 9 - PHC steps 1-29

---
+++ parallelHillClimber.py
@@ -0,0 +1,41 @@
+import constants as c
+from solution import SOLUTION
+import copy
+
+class PARALLEL_HILL_CLIMBER:
+    def __init__(self):
+        self.parents = {}
+
+        for i in range(0, c.populationSize):
+            self.parents[i] = SOLUTION()
+
+    def Evolve(self):
+        for i in self.parents:
+            self.parents[i].Evaluate("GUI")
+        # for currentGeneration in range(c.numberOfGenerations):
+        #     self.Evolve_For_One_Generation()
+
+    def Evolve_For_One_Generation(self):
+        self.Spawn()
+        self.Mutate()
+        self.child.Evaluate("DIRECT")
+        self.Print()
+        self.Select()
+
+    def Spawn(self):
+        self.child = copy.deepcopy(self.parent)
+
+    def Mutate(self):
+        self.child.Mutate()
+
+
+    def Select(self):
+        if self.parent.fitness > self.child.fitness:
+            self.parent = self.child
+
+    def Print(self):
+        print("\nparent = " + str(self.parent.fitness) + " and child = " + str(self.child.fitness))
+
+    def Show_Best(self):
+        pass
+        # self.parent.Evaluate("GUI")
