4bc9a8fd8fab1217ebd7ef4fae4aef8b7366549d
cwcrowel <cwcrowel@uvm.edu>
2025-03-24 23:19:23 -0400

    Assignment 10 - completed

--- constants.py
+++ constants.py
@@ -4,12 +4,14 @@ amplitude = numpy.pi/4
 frequency = 60
 phaseOffset = numpy.pi/3
 
-sleepTime = 0.001
+sleepTime = 0.0001
 
 targetAngles = numpy.linspace(-numpy.pi, numpy.pi, 1000)
 
 numberOfGenerations = 10
 populationSize = 10
 
-numSensorNeurons = 3
-numMotorNeurons = 2
+numSensorNeurons = 9
+numMotorNeurons = 8
+
+motorJointRange = 0.2
